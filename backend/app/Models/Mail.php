a8d04e78d978c1e89c3e28aa9ecc694644fb64c6
gyenesboti <119730836+gyenesboti@users.noreply.github.com>
2023-11-06 20:05:17 +0100

    change Mail class casts, fillable and hidden properties

--- backend/app/Models/Mail.php
+++ backend/app/Models/Mail.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Mail extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+
+    protected $casts = [
+        "is_read" => "boolean",
+        "sent" => 'datetime:Y-m-d H:i:s',
+        "created" => 'datetime:Y-m-d H:i:s'
+    ];
+
+    protected $fillable = [
+        "id_user_from",
+        "id_user_to",
+        "subject",
+        "message",
+        "is_read",
+        "sent",
+        "created",
+    ];
 }
