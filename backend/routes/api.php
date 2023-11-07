833047a74942e8de9004b4dc0b1a6859a86ad361
gyenesboti <119730836+gyenesboti@users.noreply.github.com>
2023-11-07 09:00:08 +0100

    create protected route for get sent mails by user

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MailController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -17,6 +18,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::post("/signup", [UserController::class, 'signup']);
 Route::post("/login", [UserController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get("/emails/sent/{id}", [MailController::class, 'getSentEmails']);
+});
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
