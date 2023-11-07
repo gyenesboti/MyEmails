5bdc9f932d1cc4602fe61ec8d9d503c61e1906d1
gyenesboti <119730836+gyenesboti@users.noreply.github.com>
2023-11-07 08:56:38 +0100

    create a public route for login

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post("/signup", [UserController::class, 'signup']);
+Route::post("/login", [UserController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
